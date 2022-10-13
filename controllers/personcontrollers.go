95459299f61703e2618291ecdc0564587668607a
beastthor <beastthor.ykc@gmail.com>
2022-10-13 12:34:54 -0400

    first commit

---
+++ controllers/personcontrollers.go
@@ -0,0 +1,63 @@
+package controllers
+
+import (
+	"encoding/json"
+	"go-demographics/database"
+	"go-demographics/entity"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+// GetAllPerson get all person data
+func GetAllPerson(w http.ResponseWriter, r *http.Request) {
+	var persons []entity.Person
+	database.Connector.Find(&persons)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(persons)
+
+}
+
+func GetPersonByID(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	var person entity.Person
+	database.Connector.First(&person, key)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(person)
+}
+func CreatePerson(w http.ResponseWriter, r *http.Request) {
+	requestBody, _ := ioutil.ReadAll(r.Body)
+	var person entity.Person
+	json.Unmarshal(requestBody, &person)
+
+	database.Connector.Create(person)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(person)
+
+}
+func UpdatePersonByID(w http.ResponseWriter, r *http.Request) {
+	requestBody, _ := ioutil.ReadAll(r.Body)
+	var person entity.Person
+	json.Unmarshal(requestBody, &person)
+	database.Connector.Save(&person)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(person)
+}
+
+func DeletPersonByID(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	var person entity.Person
+	id, _ := strconv.ParseInt(key, 10, 64)
+	database.Connector.Where("id = ?", id).Delete(&person)
+	w.WriteHeader(http.StatusNoContent)
+}
